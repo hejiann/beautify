8b8a082648b7a8fc5b7a0656ee10d87e1de23bb0
He Jian <hejian.he@gmail.com>
2012-07-29 21:23:43 +0800

    Change all the beautify plug-ins under the "Filters" menu instead of "Tools" menu.

--- rip-border.c
+++ rip-border.c
@@ -200,7 +200,7 @@ query (void)
                           G_N_ELEMENTS (args), 0,
                           args, NULL);
 
-  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Tools/Beautify");
+  gimp_plugin_menu_register (PLUG_IN_PROC, "<Image>/Filters/Beautify");
 }
 
 static void
