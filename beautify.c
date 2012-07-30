f03f163f8d28c4c1a30a040b2106b218c3493fde
He Jian <hejian.he@gmail.com>
2012-07-30 17:17:40 +0800

    beautify: add sunset gradient effect.

--- beautify.c
+++ beautify.c
@@ -110,6 +110,7 @@ static const BeautifyEffectType art_effects[] =
 static const BeautifyEffectType gradient_effects[] =
 {
   BEAUTIFY_EFFECT_BEAM_GRADIENT,
+  BEAUTIFY_EFFECT_SUNSET_GRADIENT,
   BEAUTIFY_EFFECT_PINK_BLUE_GRADIENT,
 };
 
@@ -1054,6 +1055,9 @@ effect_icon_new (BeautifyEffectType effect)
       break;
     case BEAUTIFY_EFFECT_COLOR_PENCIL:
       title = "Color Pencil";
+      break;
+    case BEAUTIFY_EFFECT_SUNSET_GRADIENT:
+      title = "Sunset Gradient";
       break;
     case BEAUTIFY_EFFECT_BEAM_GRADIENT:
       title = "Beam Gradient";
